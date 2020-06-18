51ee13dda87bfbd076a3b8a4b3fb384a6859d30b
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-06-17 22:33:17 -0500

    creating the form in memory using modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators, Form} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'reactive-forms';
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ){
+  }
+
+  ngOnInit(): void {
+
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: [''],
+      address: this.fb.group({
+        address1: [''],
+        address2: [''],
+        state: [''],
+        zip: ['']
+      }),
+      mobiles: this.fb.array([])
+    })
+
+  }
+
 }
